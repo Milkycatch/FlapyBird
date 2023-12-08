d25f1fe64b8b110bbf127430ebe298d60d011712
Joel Gonzalez <754537@student.dusd.net>
2023-12-08 13:51:11 -0800

    anadiendo el texto
    apenas

---
+++ Flapy Bird/Assets/scrip/controls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class controls : MonoBehaviour
+{
+   
+    public GameObject gameOverText;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI Continue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
